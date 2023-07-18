8b51cbc9d21ca42fefed1aadde3f2b7ef5863a10
arunwritings <arunkumarhr1102@gmail.com>
2023-07-18 22:21:24 +0530

    DoublyLinkedList Implementation in Java

---
+++ src/data_structures/DoublyLinkedList.java
@@ -0,0 +1,49 @@
+package data_structures;
+
+public class DoublyLinkedList {
+    class Node{
+        int data;
+        Node previous;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    Node head,tail=null;
+
+    void addNode(int data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head=tail=newNode;
+            head.previous=null;
+            tail.next=null;
+        }else {
+            tail.next=newNode;
+            newNode.previous=tail;
+            tail=newNode;
+            tail.next=null;
+        }
+    }
+    void display(){
+        Node current = head;
+        if (head == null){
+            System.out.println("List is empty....");
+        }
+        System.out.println("Elements of the List....");
+        while (current != null) {
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.addNode(10);
+        doublyLinkedList.addNode(15);
+        doublyLinkedList.addNode(20);
+        doublyLinkedList.addNode(30);
+        doublyLinkedList.display();
+    }
+}
