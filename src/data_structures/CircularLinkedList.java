1c93f166cb5868ae5b9db8be3550b3de0a66389b
arunwritings <arunkumarhr1102@gmail.com>
2023-07-19 22:08:10 +0530

    DoublyCircularLinkedList Implementation in Java

---
+++ src/data_structures/CircularLinkedList.java
@@ -0,0 +1,48 @@
+package data_structures;
+
+public class CircularLinkedList {
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data=data;
+        }
+    }
+    Node head,tail=null;
+
+    void addNode(int data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head=newNode;
+            tail=newNode;
+            newNode.next=head;
+        }else {
+            tail.next=newNode;
+            tail=newNode;
+            tail.next=head;
+        }
+        }
+    void display(){
+        Node current= head;
+        if (head==null){
+            System.out.println("List is empty....");
+        }else {
+            System.out.println("Elements of the list ...");
+            do {
+                System.out.print(" "+current.data);
+                current=current.next;
+            }while (current!=head);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.addNode(15);
+        circularLinkedList.addNode(51);
+        circularLinkedList.addNode(35);
+        circularLinkedList.display();
+    }
+    }
+
