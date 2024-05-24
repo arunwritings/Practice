8964f9ca01a0ad5a7eb0ceaa05eca78845d78f6e
arunwritings <arunkumarhr1102@gmail.com>
2024-05-24 21:32:33 +0530

    24-05-2024

---
+++ src/data_structures/linkedlists/ConvertBinaryNumberInALinkedListToInteger_1290.java
@@ -0,0 +1,27 @@
+package data_structures.linkedlists;
+
+public class ConvertBinaryNumberInALinkedListToInteger_1290 {
+
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static int getDecimalValue(ListNode head) {
+        StringBuilder binaryNumbers = new StringBuilder();
+        while (head != null) {
+            binaryNumbers.append(head.val);
+            head = head.next;
+        }
+        return Integer.parseInt(binaryNumbers.toString(), 2);
+    }
+
+    public static void main(String[] args) {
+        ListNode node3 = new ListNode(1, null);
+        ListNode node2 = new ListNode(0, node3);
+        ListNode head = new ListNode(1, node2);
+        int decimalValue = getDecimalValue(head);
+        System.out.println("Decimal value: " + decimalValue);
+    }
+}
