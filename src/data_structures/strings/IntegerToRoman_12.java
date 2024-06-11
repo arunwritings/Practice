4ace8bef82bd02a95bcd62e3fc8bc5c43837b839
Arun Kumar H R <arunkumarhr1102@gmail.com>
2024-06-11 12:32:26 +0530

    Create IntegerToRoman_12.java

---
+++ src/data_structures/strings/IntegerToRoman_12.java
@@ -0,0 +1,15 @@
+public class IntegerToRoman_12 {
+
+  public static String integerToRoman(int num) {
+    String[] units = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    String[] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    String[] thousands = {"","M","MM","MMM"};
+
+    return thousands[num/1000]+hundreds[(num%1000)/100]+tens[(num%100)/10]+units[num%10];
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(integerToRoman(1999));
+  }
+}
