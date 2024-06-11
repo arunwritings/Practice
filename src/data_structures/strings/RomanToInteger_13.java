93fd415029485cbe7fb622654ac13484121923e4
Arun Kumar H R <arunkumarhr1102@gmail.com>
2024-06-11 12:49:07 +0530

    Create RomanToInteger_13.java

---
+++ src/data_structures/strings/RomanToInteger_13.java
@@ -0,0 +1,29 @@
+public class RomanToInteger_13 {
+
+  public int romanToInteger(String romanNumber) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I',1);
+        romanMap.put('V',5);
+        romanMap.put('X',10);
+        romanMap.put('L',50);
+        romanMap.put('C',100);
+        romanMap.put('D',500);
+        romanMap.put('M',1000);
+
+        int result = 0;
+        for (int i=0;i<romanNumber.length();i++) {
+            if (i>0 && romanMap.get(romanNumber.charAt(i)) > romanMap.get(romanNumber.charAt(i-1))) {
+                result += romanMap.get(romanNumber.charAt(i)) - 2*romanMap.get(romanNumber.charAt(i-1));
+            }
+            else {
+                result += romanMap.get(romanNumber.charAt(i));
+            }
+        }
+        return result;
+    }
+  
+  public static void main(String[] args) {
+    RomanToInteger_13 romanToInteger = new RomanToInteger_13();
+    System.out.println(romanToInteger.romanToInteger("MCMXCIX"));
+  }
+}
