ffa733f0bcd18dde25a318e883fc5d828752bbee
Arun Kumar <arunkumarhr1102@gmail.com>
2025-02-01 21:55:48 +0530

    01-02-25

--- src/data_structures/arrays/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
+++ src/data_structures/arrays/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -15,52 +15,28 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
      Output: [3,4]
      **/
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
+       return new int[]{binarySearch(nums, target, true), binarySearch(nums, target, false)};
+    }
 
-        result[0] = findFirstPosition(nums, target);
-        result[1] = findLastPosition(nums, target);
-
+    private int binarySearch(int[] nums, int target, boolean findFirst) {
+        int start = 0, end = nums.length-1;
+        int result = -1;
+        while (start <= end) {
+            int middle = start + (end-start)/2;
+            if (nums[middle] < target) {
+                start = middle+1;
+            } else if (nums[middle] > target) {
+                end = middle-1;
+            } else {
+                result = middle;
+                if (findFirst) {
+                    end = middle-1;
+                } else {
+                    start = middle+1;
+                }
+            }
+        }
         return result;
-    }
-
-    private int findFirstPosition(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        int firstPos = -1;
-
-        while (start <= end) {
-            int middle = start + (end - start) / 2;
-
-            if (nums[middle] < target) {
-                start = middle + 1;
-            } else if (nums[middle] > target) {
-                end = middle - 1;
-            } else {
-                firstPos = middle;
-                end = middle - 1; // Narrow search to the left side
-            }
-        }
-
-        return firstPos;
-    }
-
-    private int findLastPosition(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
-        int lastPos = -1;
-
-        while (start <= end) {
-            int middle = start + (end - start) / 2;
-
-            if (nums[middle] < target) {
-                start = middle + 1;
-            } else if (nums[middle] > target) {
-                end = middle - 1;
-            } else {
-                lastPos = middle;
-                start = middle + 1; // Narrow search to the right side
-            }
-        }
-
-        return lastPos;
     }
 
     public static void main(String[] args) {
